ce1851f3f99009e8ba33b46f05c76298fc4616d7
Wellington Oliveira <oliveira@live.it>
2021-03-31 01:08:07 -0300

    add disqus and comment policy

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-9c42c1e69977b57b7a23.js"
+    "url": "webpack-runtime-64769ea3c2ffc350b366.js"
   },
   {
     "url": "framework-83e11af5cf111be5ad89.js"
@@ -43,7 +43,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "db3f65297edc053ede6e2645805c05e8"
+    "revision": "217fca4e43b266cb0ebaf745a009346d"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-82c63c4c056f62a6fe2f.js"
@@ -54,7 +54,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "90f75b8ebeae523a0a93f5505798e777"
+    "revision": "16297c728786d629afd3c8aa1d8a660c"
   },
   {
     "url": "polyfill-9e9c796164da25fa8bfb.js"
